09a642889982c67d68978dd667407edd46aab396
scissorhands <edward_beats>
2016-08-02 20:52:23 -0500

    testing stripe plus dependency

---
+++ dependencies.js
@@ -0,0 +1,14 @@
+var wagner = require('wagner-core');
+var Stripe = require('stripe');
+
+module.exports = function(wagner){
+  var stripe = Stripe( process.env.STRIPE_API_KEY );
+
+  wagner.factory('Stripe', function(){
+    return stripe;
+  });
+
+  return {
+    Stripe: stripe
+  }
+}
